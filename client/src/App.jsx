b120843daa124b65688b4110e0fb377886e3be0c
murlikishan8-create <murlikishan8@gmail.com>
2026-09-26 19:56:21 +0530

    Dockerize Employee Management System

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+
 import NavBar from './components/NavBar'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Dashboard from './pages/dashboard'
+import Dashboard from './pages/Dashboard'
 import Home from './pages/Home'
 
 
